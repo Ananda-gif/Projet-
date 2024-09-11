64c60c5c966cabceb6dd38beeb6177790dc1cc69
AnandaRAFIDISON <anandarafidison@gmail.com>
2024-09-11 16:39:25 +0300

    refonte du serveur

--- index2.js
+++ index2.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-//console.log("fuck you node de mes deux");
->>>>>>> ce91ca6 (Les listes de projets à faire)
-=======
->>>>>>> e184582 (Mise à jour  d'index1.js)
 const express = require("express");
 const mysql = require("mysql");
 const port = 5000;
